9cccf8a436eefc2fc5d3486a5d5182311fbe6b71
[Tiffania] <ielenatiffania@gmail.com>
2023-06-03 12:26:42 -0400

    Added comments

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
+  
 
-  // Choose the high range
+  // CHOOSE THE HIGH RANGE
   // and store in a variable
   let min = 1;
   let max = await ask("Please choose the high range: ");
-  max = parseInt(max);
+  max = parseInt(max); 
 
 
   let computerGuess = 0;
@@ -27,12 +27,14 @@ async function start() {
 
   let humanHint = null;
 
-
-
+// WHILE LOOP
+// will have the computer generate its guess until the guess matches the secret number;
   while (computerGuess !== secretNumber) {
+// With this formula the computer generates a random number, in the range indicated by the user.
     computerGuess = Math.floor((Math.random() * (max - min + 1) + min));
     humanHint = await ask(`Is it ${computerGuess}? y/n \n`);
 
+// CHECK FOR THE CORRECT COMMAND
     if (humanHint !== "y" && humanHint !== "n") {
       console.log("ERROR");
 
@@ -41,16 +43,18 @@ async function start() {
         process.exit();
       }
     }
-
+// THE COMPUTER THE COMPUTER GUESSED
       if (humanHint === "y") {
         console.log("Congratulations! You got it!!");
         
       }
 
+// THE COMPUTER DIDN'T GUESS
       else {
 
         humanHint = await ask("Is it higher (h), or lower (l)?\n");
 
+// Change of the range
         humanHint === "h" ? min = computerGuess + 1 : max = computerGuess - 1;
 
       }
