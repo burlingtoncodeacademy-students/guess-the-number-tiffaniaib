54be2489bdee2c753b0f6ac8c236bcbe2bc12f17
[Tiffania] <ielenatiffania@gmail.com>
2023-06-04 09:40:58 -0400

    Game completed. Except cheat detector

--- index.js
+++ index.js
@@ -10,54 +10,133 @@ function ask(questionText) {
 start();
 
 async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
-  
-
-  // CHOOSE THE HIGH RANGE
-  // and store in a variable
-  let min = 1;
-  let max = await ask("Please choose the high range: ");
-  max = parseInt(max); 
+  // CHOICE OF GAME 
+  let gameChoice = await ask(`
+GAME 1: you think of a number and I (the computer) try to guess it.
+GAME 2: I (the computer) pick a number, and you have to guess it.
+To play game1 insert g1, for game2 insert g2\n `);
 
 
-  let computerGuess = 0;
 
 
-  let humanHint = null;
+  while (true) {
+    if (gameChoice === "g1") {
+      let computerGuess = null;
+      let humanHint = null;
 
-// WHILE LOOP
-// will have the computer generate its guess until the guess matches the secret number;
-  while (computerGuess !== secretNumber) {
-// With this formula the computer generates a random number, in the range indicated by the user.
-    computerGuess = Math.floor((Math.random() * (max - min + 1) + min));
-    humanHint = await ask(`Is it ${computerGuess}? y/n \n`);
+      //------------------------------------------------------------------------------------------------------
+      console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
+      let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
+      console.log('You entered: ' + secretNumber);
 
-// CHECK FOR THE CORRECT COMMAND
-    if (humanHint !== "y" && humanHint !== "n") {
-      console.log("ERROR");
 
-      while (humanHint !== "y" && humanHint !== "n") {
-        humanHint = await ask("Invalid input. Try again\n");
-        process.exit();
+      // CHOOSE THE HIGH RANGE
+      // and store in a variable
+      let min = 1;
+      let max = await ask("Please choose the high range: ");
+      max = parseInt(max);
+      console.log(`I see. So you picked a number in between 1 and ${max}... what could it be?`);
+
+      // WHILE LOOP
+      // will have the computer generate its guess until the guess matches the secret number;
+      while (computerGuess !== secretNumber) {
+        // With this formula the computer generates a random number, in the range indicated by the user.
+        computerGuess = Math.floor((Math.random() * (max - min + 1) + min));
+        humanHint = await ask(`Is it ${computerGuess}? y/n \n`);
+
+        // CHECK FOR THE CORRECT COMMAND
+        if (humanHint !== "y" && humanHint !== "n") {
+          console.log("ERROR");
+
+          while (humanHint !== "y" && humanHint !== "n") {
+            humanHint = await ask("Invalid input. Try again\n");
+            process.exit();
+          }
+        }
+        // THE COMPUTER THE COMPUTER WON
+        if (humanHint === "y") {
+          console.log("You Won!!");
+          let playAgain = await ask("Do you want to play another game? Answer y or n: ");
+          // let y = true;
+          // let n = false;
+          playAgain === "y" ? start() : process.exit();
+        }
+
+        // THE COMPUTER GUESSED WRONG
+        else {
+
+          humanHint = await ask("Is it higher (h), or lower (l)?\n");
+
+          // Change of the range
+          humanHint === "h" ? min = computerGuess + 1 : max = computerGuess - 1;
+        }
       }
+
+      process.exit();
     }
-// THE COMPUTER THE COMPUTER WON
-      if (humanHint === "y") {
-        console.log("Congratulations! You got it!!");
-        process.exit();
+
+    else if (gameChoice === "g2") {
+      // VARIABLE TO KEEP TRACK OF COMPUTER'S GUESS
+      let numberOfGuesses = 0;
+      // VARIABLE TO STORE THE PLAYER'S GUESS
+      let playerGuess = null;
+
+      //-------------------------------------------------------------------
+      let min = await ask("Insert low range: ");
+      min = parseInt(min);
+      let max = await ask("Insert high range: ");
+      max = parseInt(max);
+
+
+      let secretNumber = Math.floor((Math.random() * (max - min + 1) + min));
+      console.log(`Ok, I got it. I chose a number in between ${min} and ${max}. Good luck!`);
+
+
+
+      while (playerGuess !== secretNumber) {
+
+        playerGuess = await ask("What's your guess? ");
+        playerGuess = parseInt(playerGuess);
+
+        if (playerGuess < min || playerGuess > max) {
+          console.log(`ERROR. ${playerGuess} is not in the range. Try again`);
+        }
+
+
+        else if (playerGuess > secretNumber) {
+          console.log("Lower!");
+
+          // We increment of 1 the number of guesses's value
+          numberOfGuesses++
+        }
+
+        else if (playerGuess < secretNumber) {
+          console.log(`${playerGuess} is too low. Guess higher!`);
+
+          numberOfGuesses++
+
+        }
+
+        else {
+          console.log("CONGRATULATIONS!!! You won!");
+          numberOfGuesses++
+          break
+        }
+
+
       }
 
-// THE COMPUTER GUESSED WRONG
-      else {
+      console.log(`Well played! It only took you ${numberOfGuesses} guesses to find my number`);
+      let playAgain = await ask("Do you want to play another game? Answer y or n: ");
+      // let y = true;
+      // let n = false;
+      playAgain === "y" ? start() : process.exit();
+    }
 
-        humanHint = await ask("Is it higher (h), or lower (l)?\n");
-
-// Change of the range
-        humanHint === "h" ? min = computerGuess + 1 : max = computerGuess - 1;
-      }
+    else {
+      console.log("ERROR. Please select g1 or g2");
+      gameChoice = await ask("To play game1 insert g1, for game2 insert g2\n");
+    }
   }
 
-  process.exit();
 }
